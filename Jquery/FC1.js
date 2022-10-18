9470e3783799ac4b38cdb7f5ce9a7555f740d310
Luanpadao <thanhluannguyen200101@gmail.com>
2022-10-18 11:40:17 +0700

    WebServer_SCADA_HALA

--- Jquery/FC1.js
+++ Jquery/FC1.js
@@ -15,6 +15,10 @@ $(document).ready(function()
         $("#member").hide();
         $("#intructor").hide();
         $("#banner").show();
+        bt_introduce.classList.add('active');
+        bt_control.classList.remove('active');
+        bt_member.classList.remove('active');
+        bt_intructor.classList.remove('active');
     });
 
     // sự kiện click tong quan
@@ -24,6 +28,10 @@ $(document).ready(function()
         $("#member").hide();
         $("#intructor").hide();
         $("#banner").show();
+        bt_introduce.classList.add('active');
+        bt_control.classList.remove('active');
+        bt_member.classList.remove('active');
+        bt_intructor.classList.remove('active');
     });
 
     // sự kiện click mô hình
@@ -33,6 +41,10 @@ $(document).ready(function()
         $("#member").hide();
         $("#intructor").hide();
         $("#banner").show();
+        bt_introduce.classList.add('active');
+        bt_control.classList.remove('active');
+        bt_member.classList.remove('active');
+        bt_intructor.classList.remove('active');
     });
 
     // sự kiện click control
@@ -41,6 +53,10 @@ $(document).ready(function()
         $("#control").show();
         $("#member").hide();
         $("#intructor").hide();
+        bt_control.classList.add('active');
+        bt_introduce.classList.remove('active');
+        bt_member.classList.remove('active');
+        bt_intructor.classList.remove('active');
     });
 
     //sự kiện click member
@@ -49,6 +65,10 @@ $(document).ready(function()
         $("#control").hide();
         $("#member").show();
         $("#intructor").hide();
+        bt_member.classList.add('active');
+        bt_control.classList.remove('active');
+        bt_introduce.classList.remove('active');
+        bt_intructor.classList.remove('active');
     });
     //sự kiện click member1
     $("#bt_member1").click(function(){
@@ -56,38 +76,72 @@ $(document).ready(function()
         $("#control").hide();
         $("#member").show();
         $("#intructor").hide();
+        bt_member.classList.add('active');
+        bt_control.classList.remove('active');
+        bt_introduce.classList.remove('active');
+        bt_intructor.classList.remove('active');
     });
     $("#bt_member2").click(function(){
         $("#introduce").hide();
         $("#control").hide();
         $("#member").show();
         $("#intructor").hide();
+        bt_member.classList.add('active');
+        bt_control.classList.remove('active');
+        bt_introduce.classList.remove('active');
+        bt_intructor.classList.remove('active');
     });
     $("#bt_member3").click(function(){
         $("#introduce").hide();
         $("#control").hide();
         $("#member").show();
         $("#intructor").hide();
+        bt_member.classList.add('active');
+        bt_control.classList.remove('active');
+        bt_introduce.classList.remove('active');
+        bt_intructor.classList.remove('active');
     });
     $("#bt_member4").click(function(){
         $("#introduce").hide();
         $("#control").hide();
         $("#member").show();
         $("#intructor").hide();
+        bt_member.classList.add('active');
+        bt_control.classList.remove('active');
+        bt_introduce.classList.remove('active');
+        bt_intructor.classList.remove('active');
     });
     $("#bt_intructor").click(function(){
         $("#introduce").hide();
         $("#control").hide();
         $("#member").hide();
         $("#intructor").show();
+        bt_intructor.classList.add('active');
+        bt_control.classList.remove('active');
+        bt_introduce.classList.remove('active');
+        bt_member.classList.remove('active');
     });
     $(window).scroll(function(){
-        if($(this).scrollTop()>20){
+        if($(this).scrollTop()>165){
             $("#back-to-top").show();
+            $("nav").css("position", "fixed ");
+            $("nav").css("top", "0 ");
+            $("nav").css("z-index", "10");
+            $("nav").css("--bs-navbar-brand-padding-y", "0rem");
+            $("nav").css("width", "100%");
+            // $("nav").css("opacity", "1");
         }
         else{
             $("#back-to-top").hide();
+            $(".navbar").css("position", "relative ");
+            $("nav").css("--bs-navbar-brand-padding-y", "0.3125rem");
+            $("nav").css("width", "100%");
         }
+    });
+    $(".dropdown-item").click(function(){
+        navbarResponsive.classList.remove('show');
+        // item.classList.remove('show');
+        // $(".dropdown-menu").hide();
     });
     $("#back-to-top").click(function(){
         $("body,html").animate({scrollTop:0},500);
